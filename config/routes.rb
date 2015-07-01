a230ec19344ad6e2a3107bb59f7fcc5a98f98563
Edgar Pabon <edgar.pabon@gmail.com>
2015-06-30 23:10:24 -0400

    Devise for User Authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  
   get 'welcome/index'
 
   get 'about' => 'welcome#about'
+
+  devise_for :users
+  # resources :users, only: [:update, :show]
 
   root to: 'welcome#index'
 end
