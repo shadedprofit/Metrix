44cb88fd0467e1096be36515a1f702c8b3ad99c7
Edgar Pabon <edgar.pabon@gmail.com>
2015-07-08 01:01:35 -0400

    Added API controller and routes

---
+++ app/controllers/api/events_controller.rb
@@ -0,0 +1,30 @@
+class API::EventsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  before_filter :set_access_control_headers
+
+  def create
+    registered_application = RegisteredApplication.find_by(url: request.env['HTTP_ORIGIN'])
+    render json: "Unregistered application", status: :unprocessable_entity unless registered_application.present?
+    if registered_application.present?
+      @event = registered_application.events.build(event_params)
+
+      if @event.save
+        render json: @event, status: :created
+      else
+        render @event.errors, status: :unprocessable_entity
+      end
+    end
+  end
+
+  def set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = 'Content-Type'
+  end
+
+  private 
+
+  def event_params
+    params.permit(:event_name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,16 @@ Rails.application.routes.draw do
 
   get 'about' => 'welcome#about'
 
-  resources :registered_applications
-
   devise_for :users
   resources :users, only: [:update, :show]
+
+  resources :registered_applications
+
+  namespace :api, defaults: { format: :json } do
+    
+    resources :events, only: [:create]
+    match 'create_event', to: 'events#create', via: [:options]
+  end
 
   root to: 'welcome#index'
 end
